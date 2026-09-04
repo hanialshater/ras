007e18e92d9971ec64e07659b7ba46a7fa33ae28
Hani M.M. Al-Shater <hani.alshater@gmail.com>
2026-09-04 02:26:49 +0200

    Use standalone modular RSA API

--- src/ras/__init__.py
+++ src/ras/__init__.py
@@ -5,22 +5,20 @@ from .predicates import (
     BoostedLUTModel,
     LUTFactorModel,
     add_pair_interactions,
-    compile_linear_teacher,
     fit_boosted_lut,
     learn_llr_factor,
     score_boosted,
     score_factor,
-    train_linear_teachers,
 )
 from .calibration import ScalarCalibrator, fit_scalar_calibrator
 from .composition import compose_logprob, compose_query
 from .metrics import best_f1_threshold, metric_row
+from .splits import ProtocolSplit, make_protocol_split
 
 __all__ = [
     "QuantizedSubstrate", "build_substrate", "geometry_correlation",
     "BoostedLUTModel", "LUTFactorModel", "add_pair_interactions",
-    "compile_linear_teacher", "fit_boosted_lut", "learn_llr_factor",
-    "score_boosted", "score_factor", "train_linear_teachers",
-    "ScalarCalibrator", "fit_scalar_calibrator", "compose_logprob",
-    "compose_query", "best_f1_threshold", "metric_row",
+    "fit_boosted_lut", "learn_llr_factor", "score_boosted", "score_factor",
+    "ScalarCalibrator", "fit_scalar_calibrator", "compose_logprob", "compose_query",
+    "best_f1_threshold", "metric_row", "ProtocolSplit", "make_protocol_split",
 ]
