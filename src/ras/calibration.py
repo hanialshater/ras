4de70ba0bc0b0dd3ed881e97309bf896fc06c9af
Hani M.M. Al-Shater <hani.alshater@gmail.com>
2026-09-04 02:16:31 +0200

    Add calibration module

---
+++ src/ras/calibration.py
@@ -0,0 +1,4 @@
+"""Score calibration utilities."""
+from .core import ScalarCalibrator, calibrate_score_matrices, fit_scalar_calibrator
+
+__all__ = ["ScalarCalibrator", "calibrate_score_matrices", "fit_scalar_calibrator"]
